c354229b623d697079109f2b9584d98d797609c1
NEUAI <aizhengpeng@yeah.net>
2019-04-10 16:54:28 +0800

    Update ASS and CMakeLists

--- include/ASS.h
+++ include/ASS.h
@@ -1,21 +1,200 @@
 #ifndef __ASS_H__
 #define __ASS_H__
 
+#include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class ASS {
     string path;
     bool modified;
+	struct ScriptInfo {
+		string title; // <untitle>
+		string originalScript; // <unknown>
+		string originalTranslation;
+		string originalEditing;
+		string originalTiming;
+		string synchPoint;
+		string scriptUpdatedBy;
+		string updateDetails;
+		string scriptType; // V4.00+
+		string collisions; // Normal, Reverse
+		unsigned int playResY;
+		unsigned int playResX;
+		unsigned int playDepth;
+		double timer;
+		unsigned int wrapStyle; // 0-3
+		ScriptInfo() {
+			title = "<untitle>";
+			originalScript = "<unknown>";
+			scriptType = "V4.00+";
+			collisions = "Normal";
+			playResY = 1080;
+			playResX = 1920;
+			playDepth = 0;
+			timer = 100.0;
+			wrapStyle = 0;
+		}
+	} scriptInfo;
+	struct Style {
+		string name;
+		string fontname;
+		unsigned int primaryColor[4];
+		unsigned int secondaryColor[4];
+		unsigned int outlineColor[4];
+		unsigned int backColor[4];
+		bool bold;
+		bool italic;
+		bool underline;
+		bool strikeout;
+		double scaleX;
+		double scaleY;
+		unsigned int spacing;
+		double angle;
+		unsigned int borderStyle; // 1, 3
+		unsigned int outline; // 0-4
+		unsigned int shadow; // 0-4
+		unsigned int alignment; // 1-9
+		int marginL;
+		int marginR;
+		int marginV;
+		unsigned int encoding; // 0: English (Western, ANSI), 1: Default, 128: Janpanese, 134: Chinese (Simplified), 136: Chinese (Traditional)
+		Style() {
+			memset(&primaryColor, 0, sizeof(unsigned int) * 4);
+			memset(&secondaryColor, 0, sizeof(unsigned int) * 4);
+			memset(&outlineColor, 0, sizeof(unsigned int) * 4);
+			memset(&backColor, 0, sizeof(unsigned int) * 4);
+			bold = false;
+			italic = false;
+			underline = false;
+			strikeout = false;
+			scaleX = 0.0;
+			scaleY = 0.0;
+			spacing = 0;
+			angle = 0.0;
+			borderStyle = 1;
+			outline = 1;
+			shadow = 1;
+			alignment = 2;
+			marginL = 8;
+			marginR = 8;
+			marginV = 8;
+			encoding = 1;
+		}
+	};
+	vector<Style*> v4PlusStyles;
+	struct Effect {
+		string type;
+	};
+	struct ScrollUp : public Effect {
+		int y1;
+		int y2;
+		int delay;
+		int fadeAwayHeight;
+		ScrollUp() {
+			type = "Scroll up";
+			y1 = 0;
+			y2 = 0;
+			delay = 0;
+			fadeAwayHeight = 0;
+		}
+	};
+	struct Banner : public Effect {
+		int delay;
+		bool leftToRight;
+		int fadeAwayWidth;
+		Banner() {
+			type = "Banner";
+			delay = 0;
+			leftToRight = false;
+			fadeAwayWidth = 0;
+		}
+	};
+	struct ScrollDown : public Effect {
+		int y1;
+		int y2;
+		int delay;
+		int fadeAwayHeight;
+		ScrollDown() {
+			type = "Scroll down";
+			y1 = 0;
+			y2 = 0;
+			delay = 0;
+			fadeAwayHeight = 0;
+		}
+	};
+	struct Event {
+		string type;
+		unsigned int layer;
+		unsigned int start[4];
+		unsigned int end[4];
+		Style* style;
+		string name;
+		int marginL;
+		int marginR;
+		int marginV;
+		Effect* effect;
+		string text;
+		Event() {
+			layer = 0;
+			memset(&start, 0, sizeof(unsigned int) * 4);
+			memset(&end, 0, sizeof(unsigned int) * 4);
+			style = nullptr;
+			marginL = 8;
+			marginR = 8;
+			marginV = 8;
+			effect = nullptr;
+		}
+	};
+	struct Dialogue : public Event {
+		Dialogue() {
+			type = "Dialogue";
+		}
+	};
+	struct Comment : public Event {
+		Comment() {
+			type = "Comment";
+		}
+	};
+	struct Picture : public Event {
+		Picture() {
+			type = "Picture";
+		}
+	};
+	struct Sound : public Event {
+		Sound() {
+			type = "Sound";
+		}
+	};
+	struct Movie : public Event {
+		Movie() {
+			type = "Movie";
+		}
+	};
+	struct Command : public Event {
+		Command() {
+			type = "Command";
+		}
+	};
+	vector<Event*> events;
+	struct Font {
+		string fontname;
+	};
+	vector<Font*> fonts;
+	struct Graphic {
+		string filename;
+	};
+	vector<Graphic*> graphics;
+	virtual void parseFile(ifstream& file);
     
 public:
     ASS();
     explicit ASS(const string &path);
     virtual ~ASS();
-    virtual void open(const string &path);
-    virtual void save();
-    virtual void saveAs(const string &path);
+    // virtual void save();
+    // virtual void saveAs(const string &path);
 };
 
 #endif // __ASS_H__
--- src/ASS.cpp
+++ src/ASS.cpp
@@ -1,5 +1,56 @@
 #include <ASS.h>
 
+void ASS::parseFile(ifstream& file) {
+	string line;
+	enum Section { NONE, SCRIPT_INFO, V4PLUS_STYLES, EVENTS, FONTS, GRAPHICS } section = NONE;
+	while (getline(file, line)) {
+		if (line.length() == 0 || line[0] == ';' || (line[0] == '!' && line[1] == ':')) {}
+		else if (line[0] == '[') {
+			if (line == "[Script Info]") {
+				section = SCRIPT_INFO;
+			}
+			else if (line == "[v4+ Styles]") {
+				section == V4PLUS_STYLES;
+			}
+			else if (line == "[Events]") {
+				section == EVENTS;
+			}
+			else if (line == "[Fonts]") {
+				section == FONTS;
+			}
+			else if (line == "[Graphics]") {
+				section == GRAPHICS;
+			}
+			else {
+				// error
+			}
+		}
+		else {
+			if (section == SCRIPT_INFO) {
+				if (line.find("Title: ") == 0) {
+					scriptInfo.title = line.substr(7);
+				}
+				else if (line.find("Original Script: ") == 0) {
+					scriptInfo.originalScript = line.substr(17);
+				}
+				else if (line.find("Original Translation: ") == 0) {
+					scriptInfo.originalTranslation = line.substr(22);
+				}
+				else if (line.find("Original Editing: ") == 0) {
+					scriptInfo.originalEditing = line.substr(18);
+				}
+				else if (line.find("Original Timing: ") == 0) {
+					scriptInfo.originalTiming = line.substr(17);
+				}
+				else if (line.find("Synch Point: ") == 0) {
+					scriptInfo.synchPoint = line.substr(13);
+				}
+				// @todo
+			}
+		}
+	}
+}
+
 ASS::ASS() {
     modified = false;
 }
@@ -7,16 +58,25 @@ ASS::ASS() {
 ASS::ASS(const string &path) {
     this->path = path;
     modified = false;
+	ifstream file(path);
+	if (file && file.is_open()) {
+		parseFile(file);
+		file.close();
+	}
 }
 
 ASS::~ASS() {
-
-}
-
-void ASS::open(const string &path) {
-    if (this->path == path) {
-        break;
-    } else if (modified) {
-        // save current file
-    }
+	for each (auto graphic in graphics) {
+		delete graphic;
+	}
+	for each (auto font in fonts) {
+		delete font;
+	}
+	for each (auto event in events) {
+		delete event->effect;
+		delete event;
+	}
+	for each (auto style in v4PlusStyles) {
+		delete style;
+	}
 }
